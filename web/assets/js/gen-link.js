90e4d6f4d1ce7995f1ed34f62415dcc6d86515d8
Thijs van Dijk <github@inurbanus.nl>
2022-05-25 11:01:13 +0200

    Pack extension into xpi. (Which won't install, sadly.)

--- web/assets/js/gen-link.js
+++ web/assets/js/gen-link.js
@@ -7,13 +7,6 @@
 		}
 	});
 
-	let jsu = new URL("./flatten.js", location.href);
-	jsu.searchParams.append("base", (new URL("./", location.href)).toString())
-	
-	let js = await fetch(jsu);
-	let jsb = await js.text();
-	jsb = "javascript:" + jsb; // jsb = "javascript:" + encodeURIComponent(jsb);
-
 	let p = document.createElement("P");
 	if ( me === null ) {
 		document.body.appendChild(p);
@@ -21,7 +14,26 @@
 		me.parentNode.insertBefore(p, me.nextChild);
 	}
 
+	let q = document.createElement("P");
+	p.parentNode.insertBefore(q, p);
+
+	let xpu = new URL("./doc-hoarder.xpi", location.href);
+	xpu.searchParams.append("base", (new URL("./", location.href)).toString())
 	let a = document.createElement("A");
+	a.href = xpu;
+	a.innerText = "Download extension";
+	q.appendChild(a);
+
+
+	let jsu = new URL("./flatten.js", location.href);
+	jsu.searchParams.append("base", (new URL("./", location.href)).toString())
+
+	let js = await fetch(jsu);
+	let jsb = await js.text();
+	jsb = "javascript:" + jsb; // jsb = "javascript:" + encodeURIComponent(jsb);
+
+
+	a = document.createElement("A");
 	a.setAttribute("onclick", "return false;");
 	a.href = jsb;
 	a.innerText = "HOARD";
